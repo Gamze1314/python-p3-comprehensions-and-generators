6ef0d2ec8e8bd1188bb268364368cafd92fd1ca3
gamze <gamzekandara@gmail.com>
2024-06-08 17:53:17 -0400

    update list_comprehension.py

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    # returns a list of even elements [0,8]
-    # list comprehension
-    return [num for num in num_list if (num % 2 == 0)]
+    # returns the evens, if there are no even numbers, returns an empty list
+    return [num for num in num_list if num % 2 == 0]
+    
 
-
-return_evens = return_evens([0, 3, 4, 5, 6,])
-print(return_evens)
+print(return_evens(range(1,10,2)))
 
 
 def make_exclamation(sentence_list):
@@ -15,6 +13,4 @@ def make_exclamation(sentence_list):
     return [sentence + "!" for sentence in sentence_list]
 
 
-sentences = ["This is a sentence.",
-             "This is another sentence.", "And this is the last one."]
-print(make_exclamation(sentences))
+print(make_exclamation(["Hello", "Gamze"]))
